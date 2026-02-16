71f4ac96e08a66d7869f67c89e3fb2142c12a4c0
KSeangTan <kseangtan@gmail.com>
2026-02-16 21:38:25 +0900

    pre-commit clean up
    Signed-off-by: Kok Seang Tan <kseangtan@gmail.com>

--- setup.py
+++ setup.py
@@ -2,52 +2,9 @@
 
 from setuptools import setup
 
-
-# #region agent log
-def _log(msg: str, hypothesis_id: str, **data: object) -> None:
-    import json
-
-    with open("/home/kokseang/autoware-ml/.cursor/debug.log", "a") as f:
-        f.write(
-            json.dumps(
-                {
-                    "location": "setup.py",
-                    "message": msg,
-                    "hypothesisId": hypothesis_id,
-                    "data": data,
-                    "timestamp": __import__("time").time_ns() // 1_000_000,
-                }
-            )
-            + "\n"
-        )
-
-
-# #endregion
-
-# #region agent log
-_log("setup.py executed during build", "H2")
-# #endregion
-try:
-    # #region agent log
-    _log("before import autoware_ml.ops.build", "H1")
-    # #endregion
-    from autoware_ml.ops.build import get_cmdclass, get_ext_modules
-
-    # #region agent log
-    _log("import succeeded", "H1", has_ext=True)
-    # #endregion
-    ext_modules = get_ext_modules()
-    cmdclass = get_cmdclass()
-except ModuleNotFoundError as e:
-    # #region agent log
-    _log(
-        "import skipped (isolated build), using empty ext_modules", "H1", error=str(e), runId="fix"
-    )
-    # #endregion
-    ext_modules = []
-    cmdclass = {}
+from autoware_ml.ops.build import get_cmdclass, get_ext_modules
 
 setup(
-    ext_modules=ext_modules,
-    cmdclass=cmdclass,
+    ext_modules=get_ext_modules(),
+    cmdclass=get_cmdclass(),
 )
