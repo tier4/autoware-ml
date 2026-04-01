7e77142ae249cfeebe6ffb1e09a36166ee50c2c6
Kok Seang Tan <kseangtan@gmail.com>
2026-04-01 17:00:59 +0900

    Update database configs

--- autoware_ml/databases/t4datasets/t4records_generator.py
+++ autoware_ml/databases/t4datasets/t4records_generator.py
@@ -23,9 +23,9 @@ class T4SampleRecord(BaseModel):
     scenario_id: str
     sample_id: str
     sample_index: int
-    location: str
-    vehicle_type: str
     lidar_path: str
+    location: str | None
+    vehicle_type: str | None
 
     # Lidar to ego transformation
     lidar2ego_translation: Vector3
@@ -55,9 +55,8 @@ class T4RecordsGenerator:
         """
         Initialize T4 dataset generator.
         :param database_root_path: Root path of the T4 database.
-        :param scenario: Scenario data.
+        :param scenario_data: Scenario data.
         :param max_sweeps: Max number of lidar sweeps to include, only for 3D.
-        :param scenario_index: Index of the scenario.
         :param sample_steps: Number of frames/samples to skip between each sample.
         """
         self.database_root_path = Path(database_root_path)
@@ -68,14 +67,13 @@ class T4RecordsGenerator:
 
     def _construct_t4_devkit_dataset(self) -> Tier4:
         """Construct T4 dataset."""
-        scene_root_dir_path = (
-            self.database_root_path
-            / self.scenario_data.db_version
-            / self.scenario_data.scenario_id
-            / self.scenario_data.scenario_version
-        )
+        scene_root_dir_path = (self.database_root_path /
+                               self.scenario_data.db_version /
+                               self.scenario_data.scenario_id /
+                               self.scenario_data.scenario_version)
         if not scene_root_dir_path.exists():
-            raise ValueError(f"Scene root directory {scene_root_dir_path} does not exist.")
+            raise ValueError(
+                f"Scene root directory {scene_root_dir_path} does not exist.")
         return Tier4(data_root=scene_root_dir_path, verbose=False)
 
     def generate_dataset_records(self) -> Sequence[DatasetRecord]:
@@ -84,12 +82,16 @@ class T4RecordsGenerator:
         logger.info(
             f"Generating dataset records for scenario: {self.scenario_data.scenario_id} with sample steps: {self.sample_steps} and max sweeps: {self.max_sweeps}"
         )
-        for sample_index in range(0, len(self.t4_devkit_dataset.sample), self.sample_steps):
+        for sample_index in range(0, len(self.t4_devkit_dataset.sample),
+                                  self.sample_steps):
             sample = self.t4_devkit_dataset.sample[sample_index]
-            records.append(self.extract_t4_sample_record(sample, sample_index))
+            t4_sample_record = self.extract_t4_sample_record(sample, sample_index)
+            records.append(t4_sample_record.to_dataset_record())
+
         return records
 
-    def extract_t4_sample_record(self, sample: Sample, sample_index: int) -> T4SampleRecord:
+    def extract_t4_sample_record(self, sample: Sample,
+                                 sample_index: int) -> T4SampleRecord:
         """Extract T4 sample record from a T4Dataset."""
         # First, read lidar token from the sample data
         if LidarChannel.LIDAR_TOP in sample.data:
@@ -102,10 +104,10 @@ class T4RecordsGenerator:
             )
 
         # Second, read sample data and calibrated sensor from the T4Dataset
-        sd_record: SampleData = self.t4_devkit_dataset.get("sample_data", lidar_token)
+        sd_record: SampleData = self.t4_devkit_dataset.get(
+            "sample_data", lidar_token)
         cs_record: CalibratedSensor = self.t4_devkit_dataset.get(
-            "calibrated_sensor", sd_record.calibrated_sensor_token
-        )
+            "calibrated_sensor", sd_record.calibrated_sensor_token)
         lidar_path, _, _ = self.t4_devkit_dataset.get_sample_data(lidar_token)
         # TODO (KokSeang): Extract more information, for example, boxes, from the T4Dataset.
         # Last, return the T4 sample record
